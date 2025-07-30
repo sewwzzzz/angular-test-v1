da22304d3a515a57fa2fc9690132950a8e369e8e
sewwzzzz <1192963064@qq.com>
2025-07-30 22:30:30 +0800

    feat:scroll-bar

---
+++ src/component/colorful-scrollbar/colorful-scrollbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'colorful-scrollbar',
+  imports: [],
+  templateUrl: './colorful-scrollbar.html',
+  styleUrl: './colorful-scrollbar.css'
+})
+export class ColorfulScrollbar {
+
+}
